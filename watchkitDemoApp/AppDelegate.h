ebbba3250209c6809ff2e98cdcd4c70c860e806e
arwin <admin@Arwins-MacBook-Pro.local>
2015-02-12 10:22:37 +0530

    Initial Commit

---
+++ watchkitDemoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  watchkitDemoApp
+//
+//  Created by arwin on 12/02/15.
+//  Copyright (c) 2015 IGT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ watchkitDemoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  watchkitDemoApp
+//
+//  Created by arwin on 12/02/15.
+//  Copyright (c) 2015 IGT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
